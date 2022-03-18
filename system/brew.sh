f51cdfda6b1008c105ac2353e9ccefe45139de20
Tom <tom@PBE0447.local>
2022-03-18 22:21:34 +0100

    moved aliases

---
+++ system/brew.sh
@@ -0,0 +1,71 @@
+# install homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew install git
+brew install node
+brew install wget
+# brew install z
+# brew install ag
+# brew install ack
+# brew install fd
+# brew install ffind
+# brew install fpp
+# brew install tmux
+# brew install watchman
+brew install yarn
+
+npm install -g ipt
+npm install -g http-server
+npm install -g esformatter
+npm install -g prettier
+npm install -g eslint
+# yeah, haters gonna hate
+# npm install -g replace
+
+
+brew install imageoptim
+brew install iterm2
+
+# utils
+# brew install divvy
+# brew install the-unarchiver
+
+# browsers
+brew install firefox
+brew install google-chrome
+
+# others
+# brew install limechat
+# brew install skype
+# brew install rightzoom
+# brew install vlc
+
+brew install firefox
+brew install google-chrome
+
+# quick look plugins (https://github.com/sindresorhus/quick-look-plugins)
+# brew install qlcolorcode qlstephen qlmarkdown quicklook-json qlimagesize webpquicklook suspicious-package quicklookase qlvideo
+
+# github util
+brew install gh
+# nicer git diffs
+brew install git-delta
+brew install git
+
+
+brew install imagemagick
+brew install node # This installs `npm` too using the recommended installation method
+brew install rename
+brew install tree
+brew install zopfli
+brew install ffmpeg
+
+
+brew install scrcpy # control/view android phone from PC. amazing
+brew install youtube-dl
+
+# will probably need these at some point
+# brew install automake cmake go rust
+
+# Remove outdated versions from the cellar
+brew cleanup
